e7d0c6be0a5e5c0dda0802eee847aac8636d36fb
Geoff Garbers <geoff@garbers.co.za>
2019-06-03 14:08:51 +0200

    Change from regional to global backend services.

--- backend_services.tf
+++ backend_services.tf
@@ -1,10 +1,10 @@
-resource google_compute_region_backend_service default {
+resource google_compute_backend_service default {
   count = length(var.mapping)
-  name = format("%s-%0s", var.name, var.mapping[count.index]["name"])
+  name = format("%s-%s", var.name, var.mapping[count.index]["name"])
   health_checks = var.health_checks
 
   dynamic "backend" {
-    for_each = var.mapping[count.index]["instance_groups"]
+    for_each = var.mapping[count.index]["targets"]
 
     content {
       group = backend.value
